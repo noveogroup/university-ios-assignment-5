ddbf416ec8e693ac2aecdf401519bf604d5beaa5
Alexey Ershov <alaershov@gmail.com>
2014-07-29 18:40:32 +0700

    Implement background colour change with sliders

--- lab-5-uikit/lab-5-uikit/ColoredVC.m
+++ lab-5-uikit/lab-5-uikit/ColoredVC.m
@@ -10,19 +10,26 @@
 
 
 @interface ColoredVC ()
+
 @property (nonatomic, copy) NSString *titlePrefix;
 @property (strong, nonatomic) IBOutlet UIButton *nextButton;
+
+// 0 - Red, 1 - Green, 2 - Blue
+@property (strong, nonatomic) IBOutletCollection(UISlider) NSArray *colorSliders;
+
 @end
 
 
 @implementation ColoredVC
+
+CGFloat colorValues[3];
 
 - (instancetype)initWithTitlePrefix:(NSString *)titlePrefix andDepth:(unsigned int)depth {
     self = [self initWithNibName:NSStringFromClass([self class]) bundle:[NSBundle mainBundle]];
     
     _depth = depth;
     _titlePrefix = [NSString stringWithString:titlePrefix];
-
+    
     return self;
 }
 
@@ -38,11 +45,30 @@
     
     [self setTitle:[NSString stringWithFormat:@"%@ %i", self.titlePrefix, self.depth]];
     
+    for (UISlider *slider in self.colorSliders) {
+        [self colorValueChanged:slider];
+    }
+    
 }
 
 - (IBAction)nextButtonTap:(UIButton *)sender {
-    ColoredVC *nextScreenVC = [[ColoredVC alloc] initWithTitlePrefix:self.titlePrefix andDepth:(self.depth+1)];
+    ColoredVC *nextScreenVC = [[ColoredVC alloc]
+        initWithTitlePrefix:self.titlePrefix andDepth:(self.depth+1)];
+    
     [self.navigationController pushViewController:nextScreenVC animated:YES];
+}
+
+- (IBAction)colorValueChanged:(UISlider *)sender {
+    int colorIndex = [self.colorSliders indexOfObject:sender];
+    
+    colorValues[colorIndex] = sender.value;
+    
+    self.view.backgroundColor = [UIColor
+        colorWithRed:colorValues[0]
+        green:colorValues[1]
+        blue:colorValues[2]
+        alpha:1
+    ];
 }
 
 @end
