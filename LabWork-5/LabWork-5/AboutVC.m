385b08430376dcbc9402972885607b428151f49e
Konstantin Mamaev <kostya.plhs@gmail.com>
2015-03-09 19:45:33 +0600

    Implement alert appearing on the button tap

--- LabWork-5/LabWork-5/AboutVC.m
+++ LabWork-5/LabWork-5/AboutVC.m
@@ -12,7 +12,14 @@
 }
 
 - (IBAction)alertButtonTapped:(UIButton *)sender {
-    // TODO: Implement this
+    UIAlertController *alert = [UIAlertController
+                                alertControllerWithTitle:@"Alert"
+                                message:@"Alert button has been tapped!"
+                                preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *closeAlert = [UIAlertAction
+                                 actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+    [alert addAction:closeAlert];
+    [self presentViewController:alert animated:YES completion:nil];
 }
 
 - (IBAction)moreInfoButtonTapped:(UIButton *)sender {
