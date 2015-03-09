cb0dd15a0287ff186f9110d7cdc3c9798590b81c
Alexandr Ovchinnikov <Alintorius@gmail.com>
2015-03-09 17:19:15 +0600

    Add color changing

--- LabWork5/LabWork5/ColorVC.h
+++ LabWork5/LabWork5/ColorVC.h
@@ -4,4 +4,9 @@
 
 @interface ColorVC : UIViewController
 
+@property (weak, nonatomic) IBOutlet UISlider *redSlider;
+@property (weak, nonatomic) IBOutlet UISlider *greenSlider;
+@property (weak, nonatomic) IBOutlet UISlider *blueSlider;
+@property (weak, nonatomic) IBOutlet UITextField *colorTextField;
+
 @end
--- LabWork5/LabWork5/ColorVC.m
+++ LabWork5/LabWork5/ColorVC.m
@@ -8,13 +8,22 @@
 
 @implementation ColorVC
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     
     self.navigationItem.title = [NSString stringWithFormat:@"ColorVC (%d)",[self.navigationController.viewControllers indexOfObject:self]];
     
     UIBarButtonItem *next = [[UIBarButtonItem alloc]initWithTitle:@"Next" style:UIBarButtonItemStylePlain target:self action:@selector(nextButtonTapped)];
     self.navigationItem.rightBarButtonItem = next;
+    
+    [self setBackgroundColorWithRed:[self.redSlider value] green:[self.greenSlider value] blue:[self.blueSlider value]];
+    
+    [self.redSlider addTarget:self action:@selector(sliderValueChanged) forControlEvents:UIControlEventValueChanged];
+    [self.greenSlider addTarget:self action:@selector(sliderValueChanged) forControlEvents:UIControlEventValueChanged];
+    [self.blueSlider addTarget:self action:@selector(sliderValueChanged) forControlEvents:UIControlEventValueChanged];
+    
+    self.colorTextField.text = [self hexStringForColor:self.view.backgroundColor];
 
 }
 
@@ -24,6 +33,26 @@
     [self.navigationController pushViewController:newVC animated:YES];
 }
 
+- (void)setBackgroundColorWithRed:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue
+{
+    self.view.backgroundColor = [UIColor colorWithRed:red green:green blue:blue alpha:1.0];
+}
+
+- (void)sliderValueChanged
+{
+    [self setBackgroundColorWithRed:[self.redSlider value] green:[self.greenSlider value] blue:[self.blueSlider value]];
+    self.colorTextField.text = [self hexStringForColor:self.view.backgroundColor];
+}
+
+- (NSString *)hexStringForColor:(UIColor *)color
+{
+    const CGFloat *components = CGColorGetComponents(color.CGColor);
+    CGFloat r = components[0];
+    CGFloat g = components[1];
+    CGFloat b = components[2];
+    NSString *hexString=[NSString stringWithFormat:@"%02X%02X%02X", (int)(r * 255), (int)(g * 255), (int)(b * 255)];
+    return hexString;
+}
 
 
 @end
