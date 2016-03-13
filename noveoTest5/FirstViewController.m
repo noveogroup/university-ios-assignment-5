7d8e0c750fd5e21ad67a2c9436668385b9362dc1
Alisa Denisova <alideni@MacBook-Pro-AliDeni.local>
2016-03-13 19:14:52 +0600

    добавлен код задержки переключения свича

--- noveoTest5/FirstViewController.m
+++ noveoTest5/FirstViewController.m
@@ -45,7 +45,7 @@
     self.redComponentSlider.enabled = self.greenComponentSlider.enabled = self.blueComponentSlider.enabled = sender.isOn;
     
     [[UIApplication sharedApplication] beginIgnoringInteractionEvents];
-    double delayInSeconds = 0.4f;
+    double delayInSeconds = 1.0f;
     dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
     dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
         
