38efb2055d14ce24cd395ac1f048a467d09621af
Tatyana Khokhlova <tyh94@mail.ru>
2016-07-12 16:18:53 +0600

    Init

---
+++ university-ios-assignment-5/MainViewController.h
@@ -0,0 +1,15 @@
+//
+//  MainViewController.h
+//  university-ios-assignment-5
+//
+//  Created by admin on 12/07/16.
+//  Copyright © 2016 tanya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+
+@end
+
---
+++ university-ios-assignment-5/MainViewController.m
@@ -0,0 +1,27 @@
+//
+//  MainViewController.m
+//  university-ios-assignment-5
+//
+//  Created by admin on 12/07/16.
+//  Copyright © 2016 tanya. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
